04650994a24f55ed7afe03a09f9aceea297b61eb
daemin123 <dnjsekdl1@nate.com>
2024-03-13 18:05:32 +0900

    Initial commit

---
+++ src/main/java/com/yeonseo/YeonseoApplication.java
@@ -0,0 +1,13 @@
+package com.yeonseo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YeonseoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YeonseoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/yeonseo/YeonseoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yeonseo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YeonseoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
